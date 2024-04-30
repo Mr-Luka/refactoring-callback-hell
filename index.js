45fa67a058339bdf22bdba7495548422bfcae067
Luka <luka.luka.luka93@gmail.com>
2024-04-29 18:25:12 -0700

    making wait function to fix the christmas tree code

--- index.js
+++ index.js
@@ -9,24 +9,57 @@
 
 const go = document.querySelector(".go");
 
-go.addEventListener("click", function(e){
-    const target = e.currentTarget;
-    target.textContent = "Go!"
-    setTimeout (function (){
-        go.classList.add("circle");
-        setTimeout(function(){
-            go.classList.add("red")
-            setTimeout(function(){
-                go.classList.remove("circle");
-                setTimeout(function(){
-                    go.classList.remove("red")
-                    go.classList.add("purple");
-                    setTimeout(function(){
-                        go.classList.add("timeOut")
-                    },500)
-                }, 300)
-            },250)
-        },500)
-    },1000)
 
-});
+
+const wait = (ms= 0)=> new Promise(resolve => setTimeout(resolve, ms))
+
+wait(2000).then(()=>console.log(`Done!`))
+
+function animate(e){
+    const el = e.currentTarget;
+    el.textContent = "Go!"
+
+    wait(200)
+    .then(()=>{
+        el.classList.add("circle");
+        return wait(500)
+    })
+    .then(()=>{
+        el.classList.add("red")
+        return wait(250)
+    })
+    .then(()=>{
+        el.classList.remove("circle");
+        return wait(500);
+    })
+    .then(()=>{
+        el.classList.remove("red")
+        el.classList.add("purple");
+        return wait(500)
+    })
+    .then(()=>{
+        el.classList.add("timeOut")
+    })
+    
+}
+go.addEventListener("click", animate)
+// go.addEventListener("clickxxx", function(e){
+//     const el = e.currentTarget;
+//     el.textContent = "Go!"
+//     setTimeout (function (){
+//         el.classList.add("circle");
+//         setTimeout(function(){
+//             el.classList.add("red")
+//             setTimeout(function(){
+//                 el.classList.remove("circle");
+//                 setTimeout(function(){
+//                     el.classList.remove("red")
+//                     el.classList.add("purple");
+//                     setTimeout(function(){
+//                         el.classList.add("timeOut")
+//                     },500)
+//                 }, 300)
+//             },250)
+//         },500)
+//     },1000)
+// });
